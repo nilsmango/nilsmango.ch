eae1d772aecd72b1e1c72b9ce10fcc190114bc72
nilsmango <simonmilagrolang@icloud.com>
2026-02-22 16:54:39 +0100

    mango deploy made better

--- deploy_mango.sh
+++ deploy_mango.sh
@@ -2,9 +2,5 @@
 # sensitive info saved in the config
 source deploy_config.sh
 
-# FTP Deployment using lftp
-lftp -c "
-set ssl:verify-certificate no
-open -u $USER,$PASSWORD $HOST
-mirror -R --delete --verbose $LOCAL_PATH $REMOTE_PATH
-"
+# SSH Deployment using rsync
+rsync -rlvz --delete --no-perms --omit-dir-times -e  "ssh -i ~/.ssh/hetzner_server" $LOCAL_PATH simxn@46.225.71.191:$REMOTE_PATH
